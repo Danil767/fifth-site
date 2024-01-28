0d706748281d009d22973e94613369d91e58a49c
Danil767 <danil.an.2024@mail.ru>
2024-01-28 17:23:33 +0600

    cite adaptiv commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 $(function () {
-    let mixer = mixitup('.portfolio__content');
 
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt=""></button>' ,
-        nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt=""></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt=""></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     })
+
+    $('.menu__btn').on('click', function () {
+        $('.header__top-inner').toggleClass('header__top-inner--active')
+    })
+
+    let mixer = mixitup('.portfolio__content');
 })
